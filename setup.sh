005999eaaae7b353f887bb48928c41a769b498df
Sahil Narain <sahil.narain@gmail.com>
2016-08-24 19:37:42 +0530

    Major colorscheme revamp!

--- setup.sh
+++ setup.sh
@@ -48,7 +48,7 @@ git clone https://github.com/tomtom/tcomment_vim.git ~/.vim/bundle/tcomment
 git clone https://github.com/airblade/vim-gitgutter.git ~/.vim/bundle/vim-gitgutter
 git clone https://github.com/ervandew/supertab.git ~/.vim/bundle/supertab
 cp $curDir/colors/* ~/.vim/colors/
-echo 'colorscheme janah' >> ~/.vimrc
+echo 'colorscheme made_of_code' >> ~/.vimrc
 
 echo 'let mapleader = ","' >> ~/.vimrc
 echo ':nnoremap <leader>/ :TComment<Enter>' >> ~/.vimrc
